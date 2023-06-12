2311fe1a739bbfaafe6c93ccee75de9ead5a5d5f
Zara <zarabucknor@yahoo.co.uk>
2023-06-12 15:44:51 +0100

    Challenge complete

---
+++ 05_conditionals/conditionals-challenge.js
@@ -0,0 +1,18 @@
+const fizzBuzz = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'fizzBuzz';
+    } else if (num % 3 === 0) {
+        return 'fizz';
+    } else if (num % 5 === 0) {
+        return 'buzz';
+    } else {
+        return num;
+    }
+}
+
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(8));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(18));
+console.log(fizzBuzz(20));
