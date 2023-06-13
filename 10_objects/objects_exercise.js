08fa9daedb2fac6aaa8a7ee8e6af4743a2657002
Zara <zarabucknor@yahoo.co.uk>
2023-06-13 15:42:57 +0100

    called information from an object

---
+++ 10_objects/objects_exercise.js
@@ -0,0 +1,12 @@
+const person = {
+  name: 'Maxine',
+  age: 32,
+  address: {
+    city: 'London',
+    postcode: 'E1 123'
+  },
+  hobbies: ['writing', 'tennis', 'cooking']
+};
+
+console.log(person.address.city)
+console.log(person.hobbies[1])
