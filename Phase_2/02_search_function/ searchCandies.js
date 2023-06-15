1f625367535cd5796ba116354e7dba41ba2f2e23
Zara <zarabucknor@yahoo.co.uk>
2023-06-15 16:16:29 +0100

    Initial commit

---
+++ Phase_2/02_search_function/ searchCandies.js
@@ -0,0 +1,24 @@
+const candies = [
+  { name: 'Aero', price: 1.99 },
+  { name: 'Skitties', price: 2.99 },
+  { name: 'Mars', price: 1.49 },
+  { name: 'Maltesers', price: 3.49 },
+  { name: 'Skittles', price: 1.49 },
+  { name: 'Starburst', price: 5.99 },
+  { name: 'Ricola', price: 1.99 },
+  { name: 'Polkagris', price: 5.99 },
+  { name: 'Pastila', price: 4.99 },
+  { name: 'Mentos', price: 8.99 },
+  { name: 'Raffaello', price: 7.99 },
+  { name: 'Gummi bears', price: 10.99 },
+  { name: 'Fraise Tagada', price: 5.99 }
+];
+
+const searchCandies = (searchString, maxPrice) => {
+  return candies
+    .filter(candy => candy.name.toLowerCase().startsWith(searchString.toLowerCase()))
+    .filter(candy => candy.price < maxPrice)
+    .map(candy => candy.name);
+}
+
+export default searchCandies;
---
+++ Phase_2/02_search_function/searchCandies.test.js
@@ -0,0 +1,19 @@
+const searchCandies = require('/searchCandies');
+
+describe('searchCandies', () => {
+    it('search candies by first 2 character Ma and price below 10', () => {
+        expect(searchCandies).toEqual([ 'Mars', 'Maltesers' ]);
+    });
+
+    it('search candies by first 2 character (Ma) and price above 2', () => {
+        expect(searchCandies).toEqual([ 'Mars' ]);
+    });
+
+    it('search candies by first character (S) and price below 10', () => {
+        expect(searchCandies).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+    });
+
+    it('search candies by first character (S) and price more than 4', () => {
+        expect(searchCandies).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+    });
+});
